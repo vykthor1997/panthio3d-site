c824edfb2bf7f795de2204d246d4e1d7123dbf8b
João Vitor <diasjoaovitor1997@gmail.com>
2022-10-29 16:32:39 -0300

    feat: add header link to step by step

--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -12,15 +12,20 @@ export const Header: React.FC = () => {
     <>
     <Stack component="header" sx={{ ...S.header, backgroundColor: bg.header }}>
       <Box className="header-box">
-        <Typography component="h1" variant="h5">
-          Bolão da Copa 2022
-        </Typography>
-        <Typography component="h2" variant="subtitle1" sx={{ backgroundColor: bg.box }}>
-          Organizador: 
-          <Link href="https://panthio3d.wixsite.com/bolao" target="_blank">
-            Marcelo Panthio 3D
-          </Link>
-        </Typography>
+        <Box>
+          <Typography component="h1" variant="h5">
+            Bolão da Copa 2022
+          </Typography>
+          <Typography component="h2" variant="subtitle1" sx={{ backgroundColor: bg.box }}>
+            Organizador: 
+            <Link href="https://panthio3d.wixsite.com/bolao" target="_blank">
+              Marcelo Panthio 3D
+            </Link>
+          </Typography>
+        </Box>
+        <Link href="https://panthio3d.wixsite.com/bolao/aposte" target="_blank">
+          Regras
+        </Link>
       </Box>
     </Stack>
     <Divider />
--- src/shared/components/Header/style.ts
+++ src/shared/components/Header/style.ts
@@ -3,6 +3,9 @@ import { SxProps, Theme } from "@mui/material"
 export const header: SxProps<Theme> = {
   padding: 2,
   '& .header-box': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     '& h2': {
       padding: .5,
       display: 'inline',
